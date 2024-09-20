37880f41c8eaceb6837eef7c0e3f52db4975bc96
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2024-09-20 09:32:57 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,42 @@
+print("Act 9 clases v3")
+print("Jesus Adrian Esquivel: 22308051281176")
+# Zona de clase 
+class Operadores1176:
+    def suma1176(self,J,A):
+        s1176=J+A
+        print(f"La suma de {J} + {A} = {s1176}")
+    def rest1176(Self,J,A):
+        r1176=J-A
+        print(F"La resta de {J} - {A} = {r1176}")
+    def mult1176(self,J,A):
+        mul1176=J*A
+        print(f"La multiplicacion de {J} * {A} = {mul1176}")
+    def div1176(self,J,A):
+        divi1176=J/A
+        print(F"La division de {J} / {A} = {divi1176}")
+    def mod1176(self,J,A):
+        mod1176=J%A
+        print(f"El modulo de {J} % {A} = {mod1176}")
+    def exp1176(self,J,A):
+        exp1176=J**A
+        print(f"El exponente de {J} ** {A} = {exp1176}")
+    def  coc1176(self,J,A):
+        coci1176=J//A
+        print(f"El cociente de {J} // {A} = {coci1176}")
+# Zona de creacion del objeto
+operAdrian=Operadores1176()
+# Zona de uso de objetos
+print("Funcion suma ")
+operAdrian.suma1176(1,9)
+print("Funcion resta")
+operAdrian.rest1176(9,1)
+print("Funcion Multipliacacion")
+operAdrian.mult1176(2,5)
+print("Funcion Division")
+operAdrian.div1176(20,2)
+print("Funcion Modulo")
+operAdrian.mod1176(5,5)
+print("Funcion Exponente")
+operAdrian.exp1176(2,8)
+print("Funcion Cociente")
+operAdrian.coc1176(8,7)
